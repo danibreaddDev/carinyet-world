42c78175895c15e56cb70780cd4067d8daffa770
Dani <danielpamblancomestre@gmail.com>
2026-06-18 16:21:27 +0200

    feat: added images tu public folder

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 <template>
     <RouterLink to="/profile" class="flex flex-row gap-1 items-center w-fit px-4 py-2">
 
-        <img :src="'src/assets/home/' + store.character.id + '_icon.png'" alt="" class="size-24">
+        <img :src="'/assets/home/' + store.character.id + '_icon.png'" alt="" class="size-24">
         <span class="text-lg font-primary text-pink-300">Nivel {{ store.character.level }}</span>
 
     </RouterLink>
--- src/modules/Home/HomeCard.vue
+++ src/modules/Home/HomeCard.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
     <RouterLink to="/music" class="border border-pink-300 p-5 bg-white h-56 relative flex flex-col  ">
         <h2 class="text-3xl text-pink-300 font-primary font-bold">{{ title }}</h2>
-        <img :src="'src/assets/home/' + characterId + '_' + tag + '.png'" :alt="characterId"
+        <img :src="'/assets/home/' + characterId + '_' + tag + '.png'" :alt="characterId"
             class="absolute bottom-0 right-0 size-28">
     </RouterLink>
 </template>
